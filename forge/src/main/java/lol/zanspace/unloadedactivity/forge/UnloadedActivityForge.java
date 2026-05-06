635e3c50511b5ce2585c8094f0ea125c508be2f5
Zan <69921455+InZan17@users.noreply.github.com>
2026-05-06 04:03:38 +0200

    Remove the config screen

--- forge/src/main/java/lol/zanspace/unloadedactivity/forge/UnloadedActivityForge.java
+++ forge/src/main/java/lol/zanspace/unloadedactivity/forge/UnloadedActivityForge.java
@@ -1,10 +1,6 @@
 package lol.zanspace.unloadedactivity.forge;
 
-import lol.zanspace.unloadedactivity.config.UnloadedActivityClothScreen;
-import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.ModList;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 
 import lol.zanspace.unloadedactivity.UnloadedActivity;
@@ -13,15 +9,6 @@ import lol.zanspace.unloadedactivity.UnloadedActivity;
 public final class UnloadedActivityForge {
     public UnloadedActivityForge() {
         UnloadedActivity.init();
-
         MinecraftForge.EVENT_BUS.register(new ForgeEventHandler());
-
-        if (ModList.get().isLoaded("cloth_config")) {
-            ModLoadingContext.get().registerExtensionPoint(
-                    ConfigScreenHandler.ConfigScreenFactory.class,
-                    () -> new ConfigScreenHandler.ConfigScreenFactory((mc, screen) ->
-                            new UnloadedActivityClothScreen().getScreen(screen, mc.level != null))
-            );
-        }
     }
 }
--- neoforge/src/main/java/lol/zanspace/unloadedactivity/neoforge/UnloadedActivityNeoForge.java
+++ neoforge/src/main/java/lol/zanspace/unloadedactivity/neoforge/UnloadedActivityNeoForge.java
@@ -1,38 +1,13 @@
 package lol.zanspace.unloadedactivity.neoforge;
 
-import lol.zanspace.unloadedactivity.config.UnloadedActivityClothScreen;
-import lol.zanspace.unloadedactivity.datapack.SimulationDataResource;
-import net.neoforged.fml.ModList;
-import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import lol.zanspace.unloadedactivity.UnloadedActivity;
-#if MC_VER <= MC_1_20_4
-import net.neoforged.neoforge.client.ConfigScreenHandler;
-#else
-import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
-#endif
 import static net.neoforged.neoforge.common.NeoForge.EVENT_BUS;
 
 @Mod(UnloadedActivity.MOD_ID)
 public final class UnloadedActivityNeoForge {
     public UnloadedActivityNeoForge() {
         UnloadedActivity.init();
-
         EVENT_BUS.register(new NeoForgeEventHandler());
-
-        if (ModList.get().isLoaded("cloth_config")) {
-            #if MC_VER <= MC_1_20_4
-            ModLoadingContext.get().registerExtensionPoint(
-                ConfigScreenHandler.ConfigScreenFactory.class,
-                () -> new ConfigScreenHandler.ConfigScreenFactory((mc, screen) ->
-                    new UnloadedActivityClothScreen().getScreen(screen, mc.level != null))
-            );
-            #else
-            ModLoadingContext.get().registerExtensionPoint(
-                IConfigScreenFactory.class,
-                () -> (mc, screen) -> new UnloadedActivityClothScreen().getScreen(screen, true)
-            );
-            #endif
-        }
     }
 }
